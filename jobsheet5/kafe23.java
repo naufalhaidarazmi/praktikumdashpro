1a72d57300d7d215961e8c06c97c5cca79b65a90
naufalhaidarazmi <naufalhaidar946@gmail.com>
2025-11-26 11:47:25 +0700

    kafenoabsenjava

---
+++ jobsheet5/kafe23.java
@@ -0,0 +1,22 @@
+package jobsheet5;
+
+public class kafe23 {
+
+    public static void main(String[] args) {
+        menu();
+    }
+
+    public static void menu () {
+
+        System.out.println("=======MENU RESTO KAFE=======");
+        System.out.println("1. kopi hitam - rp 15.000");
+        System.out.println("2. cappurcino - rp 20.000");
+        System.out.println("3. latte - rp 22.000");
+        System.out.println("4. teh tarik - rp 12.000");
+        System.out.println("5. roti bakar - rp 10.000");
+        System.out.println("6, mie goreng - rp 18.000");
+        System.out.println("===============================");
+        System.out.println("silahkan pilih menu yang anda inginkan ");
+
+    }
+}
